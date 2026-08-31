997ce1de2204b6f2b9e704127fe8d4c8ef5d749e
Oleksandr Ovdiienko <oleksandr.ovdik@gmail.com>
2026-09-01 02:10:08 +0300

    docs: add final README, integrate Bull Board, and update project configs

--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -3,13 +3,18 @@ import { BullModule } from '@nestjs/bullmq';
 import { ConfigModule } from '../config/config.module.js';
 import { DeliveryProducer } from './delivery.producer.js';
 import { DeliveryProcessor } from './delivery.processor.js';
+import { BullBoardModule } from '@bull-board/nestjs';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 
 @Module({
   imports: [
     BullModule.registerQueue({ name: 'order-delivery' }),
-    ConfigModule,
+    ConfigModule, BullBoardModule.forFeature({
+      name: 'order-delivery',
+      adapter: BullMQAdapter,
+    }),
   ],
   providers: [DeliveryProducer, DeliveryProcessor],
   exports: [DeliveryProducer],
 })
-export class DeliveryModule {}
+export class DeliveryModule { }
